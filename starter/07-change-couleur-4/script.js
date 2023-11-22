2b92ddca0bee680831d88b6071d7e1db5c77cdfd
Loïc Delanoë <loic.del4127@gmail.com>
2023-11-22 12:33:16 +0100

    Done ✅

--- starter/07-change-couleur-4/script.js
+++ starter/07-change-couleur-4/script.js
@@ -6,3 +6,37 @@ Version 1  : une fonction par bouton radio
 		(1) un alert dans une fonction pour voir à quoi correspond e.target
 		(2) un alert dans chaque fonction pour voir à quoi correspond e.target.value
 */
+// red.addEventListener('click', () => {
+//     body.style.backgroundColor = 'red';
+// });
+//
+// green.addEventListener('click', () => {
+//     body.style.backgroundColor = 'green';
+// });
+
+
+const color = {
+    body: document.body,
+    alpha: 1,
+    init() {
+        this.inputs = document.querySelectorAll('input[type="radio"]');
+
+        this.inputs.forEach((input) => {
+            input.addEventListener('change', () => {
+                this.body.style.backgroundColor = input.value;
+            });
+        });
+
+        this.randomBtn = document.getElementById('boutonCouleur');
+        this.randomBtn.addEventListener('click', () => {
+            this.red = Math.floor(Math.random() * 255);
+            this.green = Math.floor(Math.random() * 255);
+            this.blue = Math.floor(Math.random() * 255);
+            this.body.style.backgroundColor = `rgba(${this.red}, ${this.green}, ${this.blue}, ${this.alpha})`;
+        });
+    },
+};
+
+color.init();
+
+console.log(color.alpha);
